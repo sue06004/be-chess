885b38da1132c5eeddec65db830d6981dd410114
sue06004 <64758888+sue06004@users.noreply.github.com>
2023-07-03 22:22:38 +0900

    7/3 verify수정

--- src/test/java/softeer2nd/PawnTest.java
+++ src/test/java/softeer2nd/PawnTest.java
@@ -4,19 +4,25 @@ import org.junit.jupiter.api.*;
 import static org.assertj.core.api.Assertions.*;
 
 public class PawnTest {
-
     @Test
     @DisplayName("흑백 폰이 생성되어야 한다")
     public void create() {
         String white="white";
         String black="black";
         Pawn pawn1 = new Pawn(white);
-        //assertThat(pawn1.getColor()).isEqualTo(white);
-        pawn1.verifyPawn(white);
+        assertThat(pawn1.getColor()).isEqualTo(white);
 
         Pawn pawn2 = new Pawn(black);
-        //assertThat(pawn2.getColor()).isEqualTo(black);
-        pawn2.verifyPawn(black);
+        assertThat(pawn2.getColor()).isEqualTo(black);
+
+        verifyPawn(white);
+        verifyPawn(black);
+
+    }
+
+    public void verifyPawn(final String color){
+        Pawn pawn = new Pawn(color);
+        assertThat(pawn.getColor()).isEqualTo(color);
     }
 
 }
